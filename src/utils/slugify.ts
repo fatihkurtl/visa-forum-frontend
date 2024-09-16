97290f76082c6ebc0d62d54ed42b73b2f61a36e3
fatihkurtl <kurtf061@gmail.com>
2024-09-16 23:11:58 +0300

    slugify updated

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,6 +1,24 @@
 export function slugify(text: string): string {
-    return text
-      .toLowerCase()
-      .replace(/[^\w ]+/g, '')
-      .replace(/ +/g, '-');
-  }
+  const turkishChars: { [key: string]: string } = {
+    ı: "i",
+    ğ: "g",
+    ü: "u",
+    ş: "s",
+    ö: "o",
+    ç: "c",
+    İ: "I",
+    Ğ: "G",
+    Ü: "U",
+    Ş: "S",
+    Ö: "O",
+    Ç: "C",
+  };
+
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9\s]/g, (char) => turkishChars[char] || "")
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
